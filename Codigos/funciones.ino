6433c03854c01597b8f8ff46af3eaa4085d6cb46
JAlonsoDV <53461069+JAlonsoDV@users.noreply.github.com>
2019-09-30 20:23:36 -0300

    Add files via upload

---
+++ Codigos/funciones.ino
@@ -0,0 +1,248 @@
+void Inicio(){
+  pwm.setPWM(0, 0, pulseWidth(0));
+  pwm.setPWM(1, 1, pulseWidth(0));
+  pwm.setPWM(2, 2, pulseWidth(0));
+  pwm.setPWM(3, 3, pulseWidth(0));
+  pwm.setPWM(4, 4, pulseWidth(0));
+  pwm.setPWM(5, 5, pulseWidth(0));
+}
+
+void Cons(){
+  reloj;
+  delay(d);
+  Error;
+  delay(r);
+}
+
+void letA(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraA.wav");
+  
+  
+}
+void letB(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraB.wav");
+  
+}
+
+void letC(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraC.wav");
+  
+}
+
+void letD(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraD.wav");
+  
+}
+
+void letE(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraE.wav");
+  
+}
+
+void letF(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraF.wav");
+  
+}
+
+void letG(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraG.wav");
+  
+  
+}
+
+void letH(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraH.wav");
+  
+}
+
+void letI(){
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraI.wav");
+  
+}
+
+void letJ(){
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraJ.wav");
+
+  
+}
+
+void letK(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraK.wav");
+  
+}
+
+void letL(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraL.wav");
+  
+}
+void letM(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraM.wav");
+  
+}
+
+
+void letN(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraN.wav");
+  
+}
+
+void letO(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraO.wav");
+  
+}
+
+void letP(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraP.wav");
+
+}
+  
+void letQ(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraQ.wav");
+  
+}
+void letR(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraR.wav");
+  
+}
+void letS(){
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraS.wav");
+  
+}
+void letT(){
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraT.wav");
+  
+}
+void letU(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  pwm.setPWM(5, 5, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraU.wav");
+  
+}
+void letV(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  pwm.setPWM(5, 5, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraV.wav");
+  
+}
+void letW(){
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(2, 2, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  pwm.setPWM(5, 5, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraW.wav");
+  
+}
+void letX(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  pwm.setPWM(5, 5, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraX.wav");
+  
+}
+void letY(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(1, 1, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  pwm.setPWM(5, 5, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraY.wav");
+  
+}
+void letZ(){
+  pwm.setPWM(0, 0, pulseWidth(g));
+  pwm.setPWM(3, 3, pulseWidth(g));
+  pwm.setPWM(4, 4, pulseWidth(g));
+  pwm.setPWM(5, 5, pulseWidth(g));
+  Cons();
+  tmrpcm.play("LetraZ.wav");
+  
+}
